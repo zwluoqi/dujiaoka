5732785a5895254a678b4f6bd557c12b7e49fb31
zwluoqi <zwluoqi@gmail.com>
2025-01-10 04:23:02 -0800

    Update StripeCheckoutController.php

--- app/Http/Controllers/Pay/StripeCheckoutController.php
+++ app/Http/Controllers/Pay/StripeCheckoutController.php
@@ -46,7 +46,8 @@ class StripeCheckoutController extends PayController
                 'mode'                => 'payment',
                 'customer_email'      => $this->order->email,
                 // 'payment_method_types'=> ['alipay','wechat_pay', 'card'],
-                'payment_method_types'=> ['alipay'],
+                // 'payment_method_types'=> ['alipay'],
+                'payment_method_types'=> ['alipay','card'],
                   # Specify the client (currently, Checkout only supports a client value of "web")
                 // 'payment_method_options' => [
                 //     'wechat_pay' => [
