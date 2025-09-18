d6eb396b846e8e8054a0e879067cb59a8e5eb867
zwluoqi <zwluoqi@gmail.com>
2025-09-18 08:08:55 +0800

    support WeChatpay

--- app/Http/Controllers/Pay/StripeCheckoutController.php
+++ app/Http/Controllers/Pay/StripeCheckoutController.php
@@ -45,15 +45,15 @@ class StripeCheckoutController extends PayController
                 ]],
                 'mode'                => 'payment',
                 'customer_email'      => $this->order->email,
-                // 'payment_method_types'=> ['alipay','wechat_pay', 'card'],
+                'payment_method_types'=> ['alipay','wechat_pay', 'card'],
                 // 'payment_method_types'=> ['alipay'],
-                'payment_method_types'=> ['alipay','card'],
+                // 'payment_method_types'=> ['alipay','card'],
                   # Specify the client (currently, Checkout only supports a client value of "web")
-                // 'payment_method_options' => [
-                //     'wechat_pay' => [
-                //     'client'=> 'web',
-                //     ],
-                // ],
+                'payment_method_options' => [
+                    'wechat_pay' => [
+                    'client'=> 'web',
+                    ],
+                ],
             ]; 
             $session = Session::create($data);
                 return redirect()->away($session->url);//可以使用自定义域名
